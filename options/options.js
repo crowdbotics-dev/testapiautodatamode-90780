88cb2466b3c6b126ab737cc97948d46df8e9f081
test user <testsneh@crowdbotics.com>
2023-06-12 17:40:14 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "testapiautodatamode-90780",
-  url: "https://testapiautodatamode-90780.botics.co",
-  api: "https://testapiautodatamode-90780.botics.co/api/v1"
-}
+  "name": "testapiautodatamode-90780",
+  "url": "https://testapiautodatamode-90780.botics.co",
+  "api": "https://testapiautodatamode-90780.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
